6b52a5bd5d6b2c3f610a8d483a61d83ed35412af
Rob Bain <rjbain@usfca.edu>
2019-05-29 15:49:21 -0700

    installing material-ui

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,13 @@ import {
   NavLink,
   HashRouter
 } from "react-router-dom"
-import { Grommet } from 'grommet';
+import { Box, Grommet } from 'grommet';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import Home from "./Home"
 import About from "./About"
 import Services from "./Services"
@@ -20,19 +26,37 @@ const theme = {
   },
 };
 
+var AppBar = (props) => (
+    <Box
+      tag='header'
+      direction='row'
+      align='center'
+      justify='between'
+      background='light-2'
+      pad={{ vertical: 'small', horizontal: 'medium' }}
+      elevation='medium'
+      {...props}
+    />
+  );
+
 class Main extends Component {
   render() {
     return (
         <HashRouter>
             <div>
               <Grommet theme={theme}>
-                <h1>Simple SPA</h1>
-                <ul className="header">
-                    <li><NavLink exact to="/">Home</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/services">Services</NavLink></li>
-                    <li><NavLink to="/contact">Contact</NavLink></li>
-                </ul>
+                <AppBar>
+                  <Typography variant="h6" color="inherit" noWrap>
+                    The Researchist
+                  </Typography>
+                  <Box direction='row'>
+                    <Button><NavLink exact to="/">Home</NavLink></Button>
+                    <Button><NavLink to="/about">About</NavLink></Button>
+                    <Button><NavLink to="/services">Services</NavLink></Button>
+                    <Button><NavLink to="/contact">Contact</NavLink></Button>
+                  </Box>                  
+                </AppBar>
+
                 <div className="content">
                     <Route exact path="/" component={Home}/>
                     <Route path="/about" component={About}/>
